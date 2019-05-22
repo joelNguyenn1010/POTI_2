917cae1fabf6cee318e9e3494c06c5938094b5d4
joelNguyenn1010 <nguyenngocanh590@gmail.com>
2019-05-23 04:43:57 +1000

    first list design

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="app.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Signika+Negative&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header class="header">
+        <p class="logo">Hertz-UTS</p>
+        <p class="brand">Car Rental Center</p>
+
+        <nav class="nav">
+            <div class="nav__box">
+                <svg class="btn-reservation__icon">
+                    <use xlink:href="./icons/symbol-defs.svg#icon-shopping-cart"></use>
+                </svg>
+                <span class="nav__notification">5</span>
+            </div>
+        </nav>
+    </header>
+
+    <section class="cars">
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+            <h1 class="car__name">Name</h1>
+            <!-- <div class="car__info"> -->
+                <p class="car__price">Price <span class="car__price__value">566</span></p> 
+
+                <p class="car__availability">Available <span class="car__availability__value">True</span></p>
+            <!-- </div> -->
+            <button class="btn__add_to_cart">Add to cart</button>
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+
+        </div>
+
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+        </div>
+
+        <div class="car">
+            <img class="cars-image" src="./images/320i.jpg" />
+        </div>
+    </section>
+</body>
+
+</html>
